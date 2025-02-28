cae55e460008290f5f1ac51c41c0d01824806ed1
RoudiOlding <roudiolding040504@gmail.com>
2025-02-28 13:42:48 -0500

    finished the routes in flask

--- 4. flask/2. web02(requestIntro)/app.py
+++ 4. flask/2. web02(requestIntro)/app.py
@@ -7,5 +7,15 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.route('/hello/<username>')
+def hello(username):
+    return f'Hello: {username}!'
+
+def helloUser(user, age):
+    return f"Hello: {user}, you already have: {age}"
+
+# An alternative way to use route, it more flexible
+app.add_url_rule('/helloUser/<user>/<age>', 'helloUser', helloUser)
+
 if __name__ == '__main__':
     app.run(debug=True)
