6b0d78ab94de1e2e2c9de1adcada6bfd11967b2c
RoudiOlding <roudiolding040504@gmail.com>
2025-02-28 18:17:27 -0500

    Finished with request object

--- 4. flask/2. web02(requestIntro)/app.py
+++ 4. flask/2. web02(requestIntro)/app.py
@@ -1,3 +1,4 @@
+'''
 from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
@@ -12,10 +13,35 @@ def hello(username):
     return f'Hello: {username}!'
 
 def helloUser(user, age):
-    return f"Hello: {user}, you already have: {age}"
+    return f"Hello: {user}! You already have: {age}"
 
 # An alternative way to use route, it more flexible
 app.add_url_rule('/helloUser/<user>/<age>', 'helloUser', helloUser)
 
 if __name__ == '__main__':
     app.run(debug=True)
+'''
+
+# Importing Necessary Modules
+from flask import *
+app = Flask(__name__)
+
+# Create a Main route here
+@app.route('/')
+def input():
+	return render_template('index.html')
+
+# Create other routes here. 
+# host/passing will be the website link
+@app.route('/passing', methods=['GET', 'POST'])
+def display():
+	if request.method == 'POST':
+		result = request.form
+		
+		# Send result data to result_data HTML file
+		return render_template("result_data.html", result=result)
+
+
+# main route to start with
+if __name__ == '__main__':
+	app.run(debug=True)
